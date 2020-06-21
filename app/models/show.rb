a0c51d4df351ceeb0d9f6489629df471accb19e9
Technicallyaustin <Technicallyaustin@gmail.com>
2020-06-21 11:56:29 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,41 +1,30 @@
 class Show < ActiveRecord::Base
-    def highest_rating
-    # returns the highest value in the rating column
-    # min and max AR methods
+    def self.highest_rating
     Shows.maximum("rating")
     end
 
-    def most_popular_show
-        #returns the show with the highest rating
-        #use min and max AR methods
-        Shows.order(rating: :desc).limit(1)
+    def self.most_popular_show
+        Shows.where(rating: highest_rating).first
     end
 
-    def lowest_rating
-        #returns the lowest value in the ratings column
+    def self.lowest_rating
         Shows.minimum("rating")
     end
 
-    def least_popular_show
-        #returns the show with the lowest ratings
-        Shows.order(rating: :asc).limit(1)
+    def self.least_popular_show
+        Shows.where(rating: lowest_rating).first
     end
 
-    def ratings_sum
-        #returns the sum of all of the ratings
+    def self.ratings_sum
         Shows.sum("ratings")
     end
 
-    def popular_shows
-        #returns an array of all of the shows that have a rating greater than 5
-        # use where AR method
+    def self.popular_shows
         Shows.where("rating > 5")
     end
 
-    def shows_by_alphabetical_order
-        #returns an arrat of all of the shows sorted by alphabetical order by name.
-        #order AR method
-        Shows.all.order(name: :desc)
+    def self.shows_by_alphabetical_order
+        Shows.order(:name)
     end
 
 end
