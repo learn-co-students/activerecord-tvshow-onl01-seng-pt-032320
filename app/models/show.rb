8499a624fec9f9bacdf28a1a11ee33ae334bc966
Technicallyaustin <Technicallyaustin@gmail.com>
2020-06-21 11:57:21 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,30 @@
 class Show < ActiveRecord::Base
     def self.highest_rating
-    Shows.maximum("rating")
+    Show.maximum("rating")
     end
 
     def self.most_popular_show
-        Shows.where(rating: highest_rating).first
+        Show.where(rating: highest_rating).first
     end
 
     def self.lowest_rating
-        Shows.minimum("rating")
+        Show.minimum("rating")
     end
 
     def self.least_popular_show
-        Shows.where(rating: lowest_rating).first
+        Show.where(rating: lowest_rating).first
     end
 
     def self.ratings_sum
-        Shows.sum("ratings")
+        Show.sum("ratings")
     end
 
     def self.popular_shows
-        Shows.where("rating > 5")
+        Show.where("rating > 5")
     end
 
     def self.shows_by_alphabetical_order
-        Shows.order(:name)
+        Show.order(:name)
     end
 
 end
